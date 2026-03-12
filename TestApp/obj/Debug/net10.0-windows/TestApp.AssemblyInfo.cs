4914dfa9e7aa5a1dff3a34f2beb744f3df3cf1c0
Hari Kiran <harikiran124@gmail.com>
2026-03-12 21:27:56 +0530

    Rebuilt Standalone App

--- TestApp/obj/Debug/net10.0-windows/TestApp.AssemblyInfo.cs
+++ TestApp/obj/Debug/net10.0-windows/TestApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f275977728108bd02e6bd7735bf7b6755fb3a6ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba9c3b34c75189c7648c4e0052e491e069235246")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
